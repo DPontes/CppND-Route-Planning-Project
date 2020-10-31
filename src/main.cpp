643cc55502cbda224507c33c2cab9bd68545af4b
DPontes <dpontes@protonmail.com>
2020-10-31 11:27:10 +0100

    added introduction of starting values

--- src/main.cpp
+++ src/main.cpp
@@ -40,9 +40,9 @@ int main(int argc, const char **argv)
         std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
         osm_data_file = "../map.osm";
     }
-    
+
     std::vector<std::byte> osm_data;
- 
+
     if( osm_data.empty() && !osm_data_file.empty() ) {
         std::cout << "Reading OpenStreetMap data from the following file: " <<  osm_data_file << std::endl;
         auto data = ReadFile(osm_data_file);
@@ -51,16 +51,30 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
+
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x = 0.0;
+    float start_y = 0.0;
+    float end_x = 0.0;
+    float end_y = 0.0;
+
+    cout << "Plese enter the start and end coordinates for your path." << endl;
+    cout << "Start X value: ";
+    cin >> start_x;
+    cout << "Start Y value: ";
+    cin >> start_y;
+    cout << "End X value: ";
+    cin >> end_x;
+    cout << "End Y value: ";
+    cin >> end_y;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
