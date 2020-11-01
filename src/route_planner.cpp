1f3ce6f29d9263b12bc6df0b66772aa83618c613
DPontes <dpontes@protonmail.com>
2020-11-01 23:28:31 +0100

    started TODO4 - AddNeighbors function

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -33,7 +33,16 @@ float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
 // - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
 
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
+    // populate current_node.neighbors vector using FindNeighbors() method
+    *current_node.FindNeighbors();
 
+    for (auto& neighbor : current_node->neighbors) {
+        neighbor.parent  = current_node;
+        neighbor.h_value = CalculateHValue(*current_node);
+        neighbor.g_value += 1;
+        neighbor.visited = true;
+        open_list.emplace_back(neighbor);
+    }
 }
 
 
